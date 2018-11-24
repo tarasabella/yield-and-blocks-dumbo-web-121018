be048c05ffcf75681d4d0baaa583637a9b7bf762
Tara Sabella <tarasabella@gmail.com>
2018-11-24 20:16:32 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  
   i = 0
   while i < array.length 
   yield array[i]
@@ -10,3 +11,4 @@ end
 #start our while loop and tell it to execute code b/w while and end as long as i i sless than the length of the array 
 #we increment the value of i inside the while loop 
 # the first time through our while loop i = 0, then i = 1 and so on until i = index number of the last item in the array 
+#the bracket method grabs the value of each succesive index element as we go through our while loop yielding each to a block
