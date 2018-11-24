ac3ea98c74d9b24235e605230097c3a9c86a6711
Tara Sabella <tarasabella@gmail.com>
2018-11-24 20:19:05 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,6 +5,7 @@ def hello_t(array)
   yield array[i]
   i = i + 1 
   end 
+  array 
 end
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
@@ -17,3 +18,5 @@ end
 #we increment the value of i inside the while loop 
 # the first time through our while loop i = 0, then i = 1 and so on until i = index number of the last item in the array 
 #the bracket method grabs the value of each succesive index element as we go through our while loop yielding each to a block
+#call method with the array of names as an argument and a block that accepts a |name| parameter 
+#If the passed in name begins with the letter "T" the block puts out a greeting
