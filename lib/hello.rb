93d6db94cb32b9e5aec3c162900d799352aaac56
Tara Sabella <tarasabella@gmail.com>
2018-11-24 20:15:28 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,12 @@
 def hello_t(array)
   i = 0
   while i < array.length 
+  yield array[i]
   i = i + 1 
-end 
+  end 
 end
 
-# call your method here!
-
+# set a ccounter variable, i = 0 
+#start our while loop and tell it to execute code b/w while and end as long as i i sless than the length of the array 
+#we increment the value of i inside the while loop 
+# the first time through our while loop i = 0, then i = 1 and so on until i = index number of the last item in the array 
